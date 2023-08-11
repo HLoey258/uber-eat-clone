dab56a9a1eacf795cb5d47168eac730e06de0a19
HLOEY <default_crt@yahoo.com>
2023-08-11 19:05:25 +0700

    add back icon in detail

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StyleSheet, Text, View } from "react-native";
 import HomeScreen from "./src/screens/HomeScreen";
 import RestaurantDetailPage from "./src/screens/RestaurantDetailPage";
+import { StatusBar } from "expo-status-bar";
 export default function App() {
   return (
     <View style={styles.container}>
       {/* <HomeScreen></HomeScreen> */}
       <RestaurantDetailPage></RestaurantDetailPage>
+      <StatusBar style="light"></StatusBar>
     </View>
   );
 }
