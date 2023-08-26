1af0b6c73e4024783768ac4f1dc8de1885181560
HLOEY <default_crt@yahoo.com>
2023-08-26 15:19:48 +0700

    clean code

--- src/screens/OrderDetailScreen/OrderDetailScreen.js
+++ src/screens/OrderDetailScreen/OrderDetailScreen.js
@@ -1,46 +1,14 @@
-import { View, Text, Image, FlatList } from "react-native";
+import { FlatList } from "react-native";
 import React from "react";
-import styles from "./style";
-import { Ionicons } from "@expo/vector-icons";
-import orders from "../../../assets/data/orders.json";
-import restaurants from "../../../assets/data/restaurants.json";
 import BasketDishItem from "../../components/BasketDishItem/BasketDishItem";
-
-const order = orders[0];
-
-const OrderDetailScreenHeader = () => {
-  return (
-    <View style={styles.page}>
-      <Image
-        source={{
-          uri: order.Restaurant.image,
-        }}
-        style={styles.image}
-      ></Image>
-      {/* <View style={styles.iconContainer}>
-        <Ionicons
-          name="arrow-back-circle"
-          size={35}
-          color={"white"}
-          style={styles.imageIcon}
-        ></Ionicons>
-      </View> */}
-      <View style={styles.container}>
-        <View style={styles.restaurantDetail}>
-          <Text style={styles.title}>{order.Restaurant.name}</Text>
-          <Text style={styles.subtitle}>{order.status} &#8226; 2 days ago</Text>
-          <Text>Your Items</Text>
-        </View>
-      </View>
-    </View>
-  );
-};
+import OrderDetailHeader from "./OrderDetailHeader";
+import restaurants from "../../../assets/data/restaurants.json";
 
 export default function OrderDetailScreen() {
   return (
     <FlatList
       data={restaurants[0].dishes}
-      ListHeaderComponent={OrderDetailScreenHeader}
+      ListHeaderComponent={OrderDetailHeader}
       renderItem={({ item }) => (
         <BasketDishItem basketDish={item}></BasketDishItem>
       )}
